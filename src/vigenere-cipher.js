e769f3bae859adddd931aaf92b19de3558d31904
Timofei <tima.chaburko@mail.ru>
2022-10-16 21:51:17 +0300

    feat:add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,77 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mashine_type = true) {
+    this.mashine_type = mashine_type;
+    this.ABC = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+  if (message == undefined || key == undefined)
+    throw new Error('Incorrect arguments!');
+  let result = '';
+  let key_index = 0;
+  let nums;
+    for(let i = 0; i < message.length; i++)
+    {
+      if(this.ABC.indexOf(message[i].toLowerCase()) !=-1)
+      {
+        if(key_index == key.length)
+          key_index = 0;
+        nums = this.ABC.indexOf(message[i].toLowerCase()) + this.ABC.indexOf(key[key_index].toLowerCase());
+        if(nums >= 26)
+          nums %= 26;
+        result += this.ABC[nums];
+      }
+      else{
+        result += message[i];
+        --key_index;
+      }
+      key_index++;
+    }
+    if (this.mashine_type) {
+      return result.toUpperCase();
+    }
+    else {
+      let p = result.split(" ").map(
+        (a)=>{return a.split("").reverse().join("")}
+       ).reverse().join(" ");
+      return p.toUpperCase();
+    }
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage == undefined || key == undefined)
+      throw new Error('Incorrect arguments!');
+    let result = '';
+    let key_index = 0;
+    let nums;
+    encryptedMessage = encryptedMessage.toLowerCase() 
+    for(let i = 0; i < encryptedMessage.length; i++)
+    {
+      if(this.ABC.indexOf(encryptedMessage[i].toLowerCase()) !=-1)
+      {
+        if(key_index == key.length)
+          key_index = 0;
+        nums = this.ABC.indexOf(encryptedMessage[i].toLowerCase()) - this.ABC.indexOf(key[key_index].toLowerCase());
+        if(nums < 0)
+        {
+          nums =  26 - Math.abs(nums);
+        }
+        result += this.ABC[nums];
+      }
+      else{
+        result += encryptedMessage[i];
+        --key_index;
+      }
+      key_index++;
+    }
+    if(!this.mashine_type)
+    {
+       let p = result.split(" ").map(
+        (a)=>{return a.split("").reverse().join("")}
+       ).reverse().join(" ");
+       return p.toUpperCase();
+    }
+    return result.toUpperCase();
   }
 }
 
