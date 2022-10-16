2490f91cd80a36485ccdd24764a8ccf8d786980f
Timofei <tima.chaburko@mail.ru>
2022-10-16 17:51:54 +0300

    feat:add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function repeater(str, options) {
 
+  let {repeatTimes = 1,separator = "+",addition,additionRepeatTimes = 1,additionSeparator = '|'} = options;
+  let result = '';
+  let one_str = ''; 
+  if(repeatTimes != 0)
+  {
+    if(addition !== undefined)
+    {
+      if(additionRepeatTimes != 0 )
+      {
+        if(additionRepeatTimes !=1){
+          one_str = String(String(str) + (String(addition) + additionSeparator).repeat(+additionRepeatTimes -1));
+          one_str += String(addition)}
+        else{one_str = String(str) + String(addition);}
+      }
+      else{
+        one_str = String(str);
+      }
+    }
+    else{
+      one_str = String(str);
+    }
+      while(repeatTimes !=0)
+      {
+        if(repeatTimes !=1){result += one_str + String(separator);}
+        else{result += one_str;}
+        --repeatTimes
+      }
+    
+  }
+  else{
+    if(addition !== undefined)
+    {
+      if(additionRepeatTimes != 0)
+      {
+        
+        if(additionRepeatTimes !=1){
+          one_str = (String(String(addition) + additionSeparator).repeat(+additionRepeatTimes -1));
+          one_str += String(addition)}
+        else{one_str =String(addition);}
+      }
+      else{
+        return 'strings';
+      }
+    }
+    else{
+      return 'strings';
+    }
+
+    while(repeatTimes !=0)
+    {
+      result += one_str;
+      --repeatTimes
+    }
+  }
+  return result;
+}
 module.exports = {
   repeater
 };
